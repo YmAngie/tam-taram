1a01002cc9b6ac13626b0922d34cb5a385461960
YmAngie <a.ymydykova@gmail.com>
2017-11-12 23:20:51 +0300

    Refactor header styles, add video pop-up with FancyBox, add JQuery code for 'load more' option.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,7 @@
-// Your code here...
+$(function () {
+    $(".video-stepbystep__link").slice(0, 3).show();
+    $("#loadMoreStepByStep").click(function (e) {
+        e.preventDefault();
+        $(".video-stepbystep__link:hidden").slice(0, 3).slideDown();
+    });
+});
