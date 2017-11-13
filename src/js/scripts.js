76e9fd1a6d7f477cf884cf1098d732642c0f9a36
Angelika Ymydykova <a.ymydykova@gmail.com>
2017-11-13 18:59:24 +0300

    Add tablet and desktop responsiveness, add slick slider.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,44 @@
+// load more
 $(function () {
-    $(".video-stepbystep__link").slice(0, 3).show();
-    $("#loadMoreStepByStep").click(function (e) {
-        e.preventDefault();
-        $(".video-stepbystep__link:hidden").slice(0, 3).slideDown();
+    if($(window).width() < 768) {
+        $(".js-video-stepbystep__link").slice(0, 3).show();
+        $("#loadMoreStepByStep").click(function (e) {
+            e.preventDefault();
+            $(".js-video-stepbystep__link:hidden").slice(0, 3).slideDown();
+        });
+    }
+});
+
+// slick slider
+$(document).ready(function() {
+    $('.js-responsive').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: "unslick"
+            }
+        ]
     });
+});
+
+// slick slider fix unslick
+$(window).resize(function() {
+    $('.js-responsive').slick('resize');
+});
+
+$(window).on('orientationchange', function() {
+    $('.js-responsive').slick('resize');
 });
