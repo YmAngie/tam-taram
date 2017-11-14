dbc1fb05c871a1dbea08ff5e7c404c3639525f0b
Angelika Ymydykova <a.ymydykova@gmail.com>
2017-11-14 12:31:33 +0300

    In addition to previous commit: Add less, js for search form.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,6 +9,7 @@ $(function () {
     }
 });
 
+
 // slick slider
 $(document).ready(function() {
     $('.js-responsive').slick({
@@ -34,7 +35,7 @@ $(document).ready(function() {
     });
 });
 
-// slick slider fix unslick
+// fix unslick in slick slider
 $(window).resize(function() {
     $('.js-responsive').slick('resize');
 });
@@ -42,3 +43,75 @@ $(window).resize(function() {
 $(window).on('orientationchange', function() {
     $('.js-responsive').slick('resize');
 });
+
+
+// // find appropriate className
+// if(document.querySelector('.js-top-search__submit')) {
+//     const submitClassName = '.js-top-search__submit';
+//     const inputClassName = '.js-top-search__input';
+// }
+// if (document.querySelector('.js-bottom-search__submit')) {
+//     const submitClassName = '.js-bottom-search__submit';
+//     const inputClassName = '.js-bottom-search__input';
+// }
+
+// show-hide search top-input
+document.querySelector('.js-top-search__submit').addEventListener('click', function() {
+    event.preventDefault();
+    const element = document.querySelector('.js-top-search__input');
+    show(element, 'show', 'hide');
+});
+
+// show-hide search bottom-input
+document.querySelector('.js-bottom-search__submit').addEventListener('click', function() {
+    event.preventDefault();
+    const element = document.querySelector('.js-bottom-search__input');
+    show(element, 'show', 'hide');
+});
+
+function show(element, addClass, delClass) {
+    if (element.classList.contains('hide')) {
+        element.classList.add(addClass);
+        element.classList.remove(delClass);
+    } else {
+        element.classList.add(delClass);
+        element.classList.remove(addClass);
+    }
+}
+
+
+// submit top-form by enter key
+document.querySelector('.js-top-search__input').addEventListener("keydown", function(e) {
+    const text = document.querySelector('.js-top-search__input').value;
+    const parameters = { text: text };
+
+    const searchForm = document.getElementById('top-search');
+
+    if (e.keyCode === 13) { sendRequest(searchForm, 'http://localhost:3000/', parameters); }
+});
+
+// submit bottom-form by enter key
+document.querySelector('.js-bottom-search__input').addEventListener("keydown", function(e) {
+    const text = document.querySelector('.js-bottom-search__input').value;
+    const parameters = { text: text };
+
+    const searchForm = document.getElementById('bottom-search');
+
+    if (e.keyCode === 13) { sendRequest(searchForm, 'http://localhost:3000/', parameters); }
+});
+
+function sendRequest(form, path, parameters) {
+    event.preventDefault();
+    for (const key in parameters) {
+        if (parameters.hasOwnProperty(key)) {
+            const hiddenField = document.createElement('input');
+            hiddenField.setAttribute('type', 'hidden');
+            hiddenField.setAttribute('name', key);
+            hiddenField.setAttribute('value', parameters[key]);
+            form.appendChild(hiddenField);
+        }
+    }
+    form.submit();
+}
+
+
